ad91b4d770034d29d5577aac0feb4cb8a9d1da91
Alba Gustems <albagustems@gmail.com>
2020-08-01 12:31:56 +0200

    Edit Form Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import { Switch, Route } from 'react-router-dom'
-import Homepage from './components/Homepage';
-import Navbar from './components/Navbar';
-<<<<<<< HEAD
-import EditForm from './components/EditForm';
-=======
-import Search from './components/Search';
->>>>>>> b7e62ed027b15ca8598e61652faa9fa4828caf70
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
+import EditForm from "./components/EditForm";
+import Search from "./components/Search";
 
 function App() {
   return (
@@ -16,11 +13,8 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
-<<<<<<< HEAD
         <Route path="/edit/:id" render={(props) => <EditForm {...props} />} />
-=======
         <Route exact path="/search" render={(props) => <Search {...props} />} />
->>>>>>> b7e62ed027b15ca8598e61652faa9fa4828caf70
       </Switch>
     </div>
   );
--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,78 +1,132 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import CompleteSubmit from "./CompleteSubmit";
 
 class EditForm extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            name: '',
-            description: '',
-            imageUrl: '',
-            file: null,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      description: "",
+      imageUrl: "",
+      file: null,
+      completed: false,
+    };
+  }
 
-    componentDidMount(){
-        //fetch la data de el place del id
-    }
-
-    handleChange = (e) => {
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/places/${this.props.match.params.id}`)
+      .then((response) => {
+        console.log(response.data);
         this.setState({
-            ...this.state,
-            [e.target.name]: e.target.value
-        })
-    } 
+          name: response.data.name,
+          description: response.data.description,
+          imageUrl: response.data.imageUrl,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        //enviar datos ruta post 
-    }
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    handleFileUpload = (e) => {
-        // cambiar el state file
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const uploadData = new FormData();
+    const history = this.props.history;
+    uploadData.append("imageUrl", this.state.file);
+    uploadData.append("name", this.state.name);
+    uploadData.append("description", this.state.description);
 
-    render() {
-        return (
-            <div>
-                <h1>Edit yout experience</h1>
-                <hr/>
-                <form className="form" onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label>Title</label>
-                        <input
-                            type="text"
-                            name="title"
-                            value={this.state.title}
-                            className="form-control"
-                            onChange={this.handleChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Description</label>
-                        <input
-                            type="text"
-                            name="description"
-                            value={this.state.description}
-                            className="form-control"
-                            onChange={this.handleChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Image</label>
-                        <input
-                            className="form-control"
-                            name="imageUrl"
-                            type="file"
-                            onChange={this.handleFileUpload}
-                        />
-                    </div>
-                    <input
-                        className="btn btn-primary"
-                        type="submit"
-                        name="submit"
-                        value="Update experience"/>
-                </form>
+    axios
+      .put(
+        `http://localhost:5000/api/places/${this.props.match.params.id}`,
+        uploadData
+      )
+      .then((response) => {
+        console.log("Updated done successfully!", response.data);
+        this.setState({
+          ...this.state,
+          completed: true,
+        });
+        setTimeout(function () {
+          history.push(`/`);
+        }, 6000);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleFileUpload = (e) => {
+    this.setState({
+      ...this.state,
+      file: e.target.files[0],
+    });
+  };
+
+  render() {
+    if (this.state.completed) {
+      return <CompleteSubmit />;
+    } else {
+      return (
+        <div>
+          <h1>Edit your experience</h1>
+          <hr />
+          <form className="form" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>Title</label>
+              <small className="form-text text-muted">
+                Write a catchy title for your experience!
+              </small>
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                className="form-control"
+                onChange={this.handleChange}
+              />
             </div>
-        )
+            <div className="form-group">
+              <label>Description</label>
+              <small className="form-text text-muted">
+                Here you can write your experience with all detail :)
+              </small>
+              <textarea
+                className="form-control"
+                value={this.state.description}
+                onChange={this.handleChange}
+                name="description"
+                rows="3"
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <label>Image</label>
+              <small className="form-text text-muted">
+                Upload one of the beautiful pictures that you took during your
+                trip
+              </small>
+              <input
+                className="form-control"
+                name="imageUrl"
+                type="file"
+                onChange={this.handleFileUpload}
+              />
+            </div>
+            <input
+              className="btn btn-primary"
+              type="submit"
+              name="submit"
+              value="Update experience"
+            />
+          </form>
+        </div>
+      );
     }
+  }
 }
 
-export default EditForm
+export default EditForm;
