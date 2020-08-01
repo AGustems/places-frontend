81d005cacf6c07e0725409bd7905c0144f12652c
Alberto Correia <alberto_cpv@hotmail.com>
2020-08-01 11:40:27 +0200

    2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
-<<<<<<< HEAD
 import Admin from './components/Admin/Admin';
-=======
-<<<<<<< HEAD
 import EditForm from './components/EditForm';
-=======
 import Search from './components/Search';
->>>>>>> b7e62ed027b15ca8598e61652faa9fa4828caf70
->>>>>>> d984d756891afdaf4718b2ff8186eba21d7630a1
+
 
 function App() {
   return (
@@ -20,15 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
-<<<<<<< HEAD
         <Route exact path = "/admin" render= {(props) => <Admin {...props} />}/>
-=======
-<<<<<<< HEAD
         <Route path="/edit/:id" render={(props) => <EditForm {...props} />} />
-=======
         <Route exact path="/search" render={(props) => <Search {...props} />} />
->>>>>>> b7e62ed027b15ca8598e61652faa9fa4828caf70
->>>>>>> d984d756891afdaf4718b2ff8186eba21d7630a1
       </Switch>
     </div>
   );
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react'
 export class Admin extends Component {
     constructor(props) {
         super (props)
+        this.state = {
 
+        }
 
     }
     render() {
           const placeEdit =  this.props.places.map((place) => {
+       
                 <div>
-               <h4>this.place.name</h4>
+               <h4>{this.place.name}</h4>
                <button> Edit</button>
                <button>Delete</button>
                 </div>
@@ -18,7 +21,7 @@ export class Admin extends Component {
 
         return (
             <div>
-            
+
             <h2>Manage your places</h2>
 
                 {placeEdit}
