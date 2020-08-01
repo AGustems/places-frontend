723fc95c3290ff3259cbf03c443c1b616f0580bd
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-08-01 11:48:33 +0200

    navbar done and carrousel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export class Navbar extends Component {
     render() {
         return (
             <div>
-                <h1>navbar</h1>
+                <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+                    {/* <!-- Brand --> */}
+                    <a className="navbar-brand" href="/">Logo</a>
+
+                    {/* <!-- Links --> */}
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link" href="/">Places</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="/">List</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="/">Search</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="/">Admin</a>
+                        </li>
+
+                    
+                    </ul>
+                    </nav>
             </div>
         )
     }
