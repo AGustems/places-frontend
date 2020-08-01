ec4bb389678c8441263ff1f2ea82930518020e9b
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-08-01 11:22:09 +0200

    add place v1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
+import AddPlaces from './components/AddPlaces';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
+        <Route exact path="/Add" component={AddPlaces}/>
       </Switch>
     </div>
   );
