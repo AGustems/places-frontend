2cad547b733c4bfdc098df2550d53d11c99d1b51
Alba Gustems <albagustems@gmail.com>
2020-08-01 11:10:07 +0200

    Edit Form v1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
+import EditForm from './components/EditForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
+        <Route path="/edit/:id" render={(props) => <EditForm {...props} />} />
       </Switch>
     </div>
   );
