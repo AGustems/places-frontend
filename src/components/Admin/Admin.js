781394ec2bca42f8e6d3f34496b3788d6c2c05af
Natalia López <natalialopezyeste@gmail.com>
2020-08-01 11:49:37 +0200

    merged alberto

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export class Admin extends Component {
-    constructor(props) {
-        super (props)
+class Admin extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const placeEdit = this.props.places.map((place) => {
+      return (
+        <div>
+          <h4>this.place.name</h4>
+          <button> Edit</button>
+          <button>Delete</button>
+        </div>
+      );
+    });
 
+    return (
+      <div>
+        <h2>Manage your places</h2>
 
-    }
-    render() {
-          const placeEdit =  this.props.places.map((place) => {
-                <div>
-               <h4>this.place.name</h4>
-               <button> Edit</button>
-               <button>Delete</button>
-                </div>
-            })
-
-
-        return (
-            <div>
-            
-            <h2>Manage your places</h2>
-
-                {placeEdit}
-                
-            </div>
-        )
-    }
+        {placeEdit}
+      </div>
+    );
+  }
 }
 
-export default Admin
+export default Admin;
