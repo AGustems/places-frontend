2f84c7e7738b806826e4de2c6062ab337965b9ee
Alberto Correia <alberto_cpv@hotmail.com>
2020-08-01 11:17:58 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" render={(props) => <Homepage {...props} />} />
+        <Route exact path = "/admin" render= {(props) => <Admin {...props} />}/>
       </Switch>
     </div>
   );
