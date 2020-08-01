3e087d8475778137f33e15f671db258515639707
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-08-01 13:53:56 +0200

    add place semi-final

--- src/components/AddPlaces.js
+++ src/components/AddPlaces.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import axios from 'axios'
+import CompleteSubmit from "./CompleteSubmit";
+import LoadingInfo from "./LoadingInfo";
 
 export class AddPlaces extends Component {
     constructor(props) {
@@ -12,26 +14,41 @@ export class AddPlaces extends Component {
             direccion: '',
             name:'',
             description:'',
-            file:null
+            saved:false,
+            loading: false
         }
     }
     handleSave = (e)=>{
         e.preventDefault()
         console.log(this.state)
         const uploadData = new FormData()
+        const history = this.props.history;
         uploadData.append('imageUrl',this.state.file)
         uploadData.append('name',this.state.name)
         uploadData.append('description',this.state.description)
         uploadData.append('lat',this.state.lat)
         uploadData.append('lng',this.state.lng)
-        console.log(uploadData)
+        this.setState({
+         ...this.state,
+        loading: true,
+        });
         axios.post('http://localhost:5000/api/places', uploadData).then(
             response=>{
                 console.log('File upload successful:', response.data)
-                
+                this.setState({...this.state,loading: false,saved:true})
+                setTimeout(function () {
+                    history.push(`/`);
+                     }, 6000);
             }
         )
     }
+    
+    handleFileUpload = (e) => {
+    this.setState({
+      ...this.state,
+      file: e.target.files[0],
+    });
+  };
 
     handleChange =(e)=>{
         this.setState({
@@ -55,6 +72,7 @@ export class AddPlaces extends Component {
                 lng: response.data.candidates[0].geometry.location.lng,
                 direccion: response.data.candidates[0].formatted_address
             })
+          
         })
     }
 
@@ -86,6 +104,12 @@ export class AddPlaces extends Component {
                 map,
                 title: this.state.direccion})
         }
+        if (this.state.loading) {
+        return <LoadingInfo />;
+        } else if (this.state.completed) {
+        return <CompleteSubmit />;
+        } else {
+     
         return (
             <div>
                 <h1>Add Place</h1>
@@ -123,7 +147,7 @@ export class AddPlaces extends Component {
                     </div>
                 </div>
             </div>
-        )
+        )}
     }
 }
 
